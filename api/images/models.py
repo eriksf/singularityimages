4db12884360597e6548178c813143d7ee103ca00
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2017-07-25 15:53:34 -0500

    name field is now unique

--- api/images/models.py
+++ api/images/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     size_in_bytes = models.BigIntegerField()
     version = models.CharField(max_length=25)
     created = models.DateTimeField()
